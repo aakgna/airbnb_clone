e600323e5a77bfdc8c5f6b7e77674d8f077366dd
Aakash Gnanakumar <aakashappiah@gmail.com>
2023-05-21 15:55:47 -0700

    Created the navbar and some registration elements

---
+++ airbnb-clone/app/layout.tsx
@@ -0,0 +1,37 @@
+import './globals.css'
+import {Nunito} from 'next/font/google'
+import Navbar from './components/navbar/Navbar'
+import ClientOnly from './components/ClientOnly'
+import RegisterModal from './components/modals/RegisterModal'
+
+export const metadata = {
+  title: "Airbnb",
+  description: 'Airbnb clone'
+}
+
+const font = Nunito({
+  subsets: ["latin"],
+})
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body className={font.className}>
+        <ClientOnly>
+          <RegisterModal />
+          <Navbar />
+        </ClientOnly>
+        {children}
+      </body>
+    </html>
+  )
+}
